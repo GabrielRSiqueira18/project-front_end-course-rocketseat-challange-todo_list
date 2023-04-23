05135221e2f86fb7c0a499f7cb5aa5e1700f18e9
GabrielRSiqueira18 <128639376+GabrielRSiqueira18@users.noreply.github.com>
2023-04-23 10:36:27 -0300

    Base ptojeto feita

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/defaults"
+import { GlobalStyle } from "./styles/global"
+import { Header } from "./components/Header"
+import { Form } from "./components/Form"
+import { Main } from './components/Main/index';
+
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Header/>
+      <Form/>
+      <Main/>
+
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
 

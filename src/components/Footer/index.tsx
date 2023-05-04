f7e43b9cd262209c87fd4f72af9a4b97de79c7c6
GabrielRSiqueira18 <128639376+GabrielRSiqueira18@users.noreply.github.com>
2023-05-04 16:56:17 -0300

    Adcionando links footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { FooterContainer } from "./styles";
 export function Footer() {
   return (
     <FooterContainer>
-      <a target="_blank" href="#">Code in github</a>
+      <a target="_blank" href="https://github.com/GabrielRSiqueira18/todo-list-final-version-rocktseat">Code in github</a>
       <a target="_blank" title="https://github.com/GabrielRSiqueira18" href="https://github.com/GabrielRSiqueira18">My profile Github</a>
     </FooterContainer>
   )
